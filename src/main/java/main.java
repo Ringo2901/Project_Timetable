b7eda322760020f92df77093176d71bc2a297a1e
Owllark <70322536+Owllark@users.noreply.github.com>
2021-10-18 20:08:58 +0300

    changing main function and adding files for data storage

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,36 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //boolean firstRun = false;
+        FileReader in = null;   //классы, хранящие инфу о том в какие файлы осуществляется ввод/вывод
+        FileWriter out = null;
+
+        File fileTimetable = new File("timetable.txt"), // файлы в которых хранятся данные
+                fileMarks = new File("marks.txt"),
+                fileTeachers = new File("teachers.txt");
+
+        int functionNumber;    // номер выбранной команды
+        Scanner input = new Scanner (System.in); // Сканнер для ввода в консоль
+        System.out.println("Enter text");// вывод начального текста(можно также сделать из файла)
+
+        functionNumber = input.nextInt(); // ввод номера функции
+        //System.out.println(functionNumber);
+
+        switch (functionNumber) {
+            case  (1):
+                break;
+            default:
+                System.out.println("This function isn't existing yet :(");
+                break;
+        }
+
+
     }
+
 }
