cb39d142f84fb9e64cd84535ef8cbce71276f1c4
Owllark <70322536+Owllark@users.noreply.github.com>
2021-10-20 22:53:05 +0300

    Updating rewrite method

---
+++ src/main/java/Rewriter.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Rewriter
+{
+    public static void rewrite(String filePath, int idx, String newLine) throws IOException {
+        Scanner sc = new Scanner(new File(filePath));
+        StringBuilder builder = new StringBuilder(); //переменная хранящая весь текст
+        String oldLine = "";
+        int i = 0;
+        while (sc.hasNextLine()) {  //запись текста
+            i++;
+            String s = sc.nextLine();
+            if(idx == i)  builder.append(newLine + System.lineSeparator()); // замена необходимой строки
+            else builder.append(s + System.lineSeparator());
+        }
+
+        sc.close();
+        if(idx > i){
+            for(int j = i; j < idx - 1; j++) {builder.append(System.lineSeparator());} // добавление пустых срок
+            builder.append(newLine + System.lineSeparator());// добавление нового пункта
+        }
+        String fileContents = builder.toString();
+
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(filePath);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+        }
+        finally {
+            // блок кода, который должен быть выполнен после завершения блока try
+        }
+        //System.out.println("new data: "+ fileContents);//иллюстративный вывод нового содержания
+        writer.append(fileContents);//запись в файл нового текста
+        writer.flush();
+        writer.close();
+
+    }
+}
