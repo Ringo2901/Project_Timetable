717115c760f05c110aca67d7381cd835ce064a34
Ringo2901 <nekitale@yandex.ru>
2021-11-22 23:10:38 +0300

    Adding testing class for tests)

---
+++ src/main/java/com/example/project_timetable/Some.java
@@ -0,0 +1,13 @@
+package com.example.project_timetable;
+
+public class Some {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ src/test/java/com/example/project_timetable/SomeTest.java
@@ -0,0 +1,18 @@
+package com.example.project_timetable;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static java.time.Duration.ofSeconds;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeTest {
+
+    @Test
+    void getNumber() {
+        Some obj = new Some();
+        obj.setNumber(5);
+        assertEquals(5, obj.getNumber());
+    }
+}
