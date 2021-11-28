7f146bdb0aaa8b5cb293cd136081a12f967ce1d8
Ringo2901 <nekitale@yandex.ru>
2021-11-29 02:42:56 +0300

    fixing tests

--- src/test/java/com/example/project_timetable/TaskListTest.java
+++ src/test/java/com/example/project_timetable/TaskListTest.java
@@ -1,7 +1,7 @@
 package com.example.project_timetable;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
