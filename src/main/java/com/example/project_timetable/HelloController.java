eca655e65fe7d0d4af1e3f1f6371aa65ca71b5d9
Ejevika <loziukvika10@gmail.com>
2021-11-28 13:34:37 +0300

    generating javadoc for 'HelloController'

--- src/main/java/com/example/project_timetable/HelloController.java
+++ src/main/java/com/example/project_timetable/HelloController.java
@@ -3,10 +3,19 @@ package com.example.project_timetable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+/**
+ * The class calls the scene
+ */
 public class HelloController {
+    /**
+     * the useless text
+     */
     @FXML
     private Label welcomeText;
 
+    /**
+     * the useless button
+     */
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
